85664b1cc427a9157c78d286aa518a0f006eb08b
Lonny Snyder <lonnysnyder81@gmail.com>
2017-05-17 18:38:18 -0500

    more practice exercises complete

---
+++ src/org/launchcode/java/exercises/ArraySum.java
@@ -0,0 +1,13 @@
+package org.launchcode.java.exercises;
+import java.util.ArrayList;
+
+/**
+ * Created by lonny on 5/17/2017.
+ */
+public class ArraySum {
+
+    public static void main(String[] args){
+
+
+    }
+}
