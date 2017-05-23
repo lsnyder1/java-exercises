659fa47298c145ff9e3ef06a8a1b0154b9bc3d0d
Lonny Snyder <lonnysnyder81@gmail.com>
2017-05-23 18:24:50 -0500

    finished arraySum class

--- src/org/launchcode/java/exercises/ArraySum.java
+++ src/org/launchcode/java/exercises/ArraySum.java
@@ -1,5 +1,6 @@
 package org.launchcode.java.exercises;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by lonny on 5/17/2017.
@@ -7,6 +8,18 @@ import java.util.ArrayList;
 public class ArraySum {
 
     public static void main(String[] args){
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        numbers.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+
+        int sum = 0;
+
+        for(Integer nums : numbers){
+            sum+=nums;
+        }
+
+        System.out.println(sum);
+
 
 
     }
